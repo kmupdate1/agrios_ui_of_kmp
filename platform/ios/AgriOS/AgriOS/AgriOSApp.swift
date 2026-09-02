d8b48d78d925b7d8e10dbd0f6d55dd89942780ee
Ken Murase <lax.potcx1562@gmail.com>
2026-09-03 06:41:08 +0900

    feat(ios): configure proper platform/ios structure

---
+++ platform/ios/AgriOS/AgriOS/AgriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgriOSApp.swift
+//  AgriOS
+//
+//  Created by Ken Murase on 2026/09/02.
+//
+
+import SwiftUI
+
+@main
+struct AgriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ platform/ios/AgriOS/AgriOS/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  AgriOS
+//
+//  Created by Ken Murase on 2026/09/02.
+//
+
+import SwiftUI
+import AgriOSKit
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
